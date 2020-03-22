a6d2d295c8f245f8c4225e1ea9c8cb0e2e3592be
Bruker <Russ2019@example.com>
2020-03-22 18:06:59 +0100

    #7

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    api3
                 </div>
 
                 <div class="links">
